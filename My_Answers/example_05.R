742adb1ebb31f6c8c1d570508c83e4f83b4f884a
szayed <54649152+szayed174@users.noreply.github.com>
2019-09-17 21:16:18 -0400

    Create example_05.R

---
+++ My_Answers/example_05.R
@@ -0,0 +1,9 @@
+#######Write an R function that will count all the even numbers up to a user defined stopping point.#####
+int <- x, count, even;
+x = 0;
+even = 0;
+if(x < count)
+ even = even+2;
+ x = x+1;
+
+###END###
