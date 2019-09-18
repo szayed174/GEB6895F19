cf4a359f3d085f40a6d2dc79bcc4274b1789efeb
szayed <54649152+szayed174@users.noreply.github.com>
2019-09-17 20:32:38 -0400

    Update and rename Answers_Assignment1_szayed to example_01.R

---
+++ My_Answers/example_01.R
@@ -0,0 +1,12 @@
+#####Write an R function that reads two numbers, multiplies them together and prints out their
+product. You could start out with multiply two <- function(num 1, num 2) { ... }######
+
+multiply_two <- function(num 1, num 2)
+{
+
+multiply <- (num_1*num_2)
+
+return (multiply)
+}
+
+####End#####
