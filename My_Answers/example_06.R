073e0a7fca4364e94f6bfaab99ec2c6891c1362d
szayed <54649152+szayed174@users.noreply.github.com>
2019-09-17 21:22:29 -0400

    Create example_06.R

---
+++ My_Answers/example_06.R
@@ -0,0 +1,48 @@
+##########Write an R function that will perform the following.
+a) Read in 5 separate numbers.
+b) Calculate the average of the five numbers.
+c) Find the smallest (minimum) and largest (maximum) of the five entered numbers.
+d) Write out the results found from steps b and c with a message describing the results,
+i.e. maximum is######
+
+#a#
+readline(prompt="please enter 5 numbers")
+seq <- n1,n2,n3,n4,n5
+
+#b#
+readline(prompt="The average is")
+{
+avg(n1+n2+n3+n4+n5)/5
+avg <- If(n1 < n2)
+ 
+ #c#
+ Set max to n2
+Else
+ Set max to n1
+If(n3 > max)
+ Set max to n3
+If(n4 > max)
+ Set max to n4
+If(n5 > max)
+Set max to n5
+
+readline(prompt="The max is")
+Write max
+If(n1 > n2)
+ Set min to n2
+Else
+ Set min to n1
+If(n3 < min)
+ Set min to n3
+If(n4 < min)
+ Set min to n4
+If(n5 < min)
+Set min to n5
+Write "The min is"
+Write min
+
+
+
+
+
+
